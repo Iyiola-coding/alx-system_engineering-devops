fa7aa81b96893bc8b2181403f1a6b48c13677058
Iyiola-coding <iyiolache@gmail.com>
2025-01-10 06:54:47 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,6 +4,6 @@ file {'iyitech_coder':
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  group   => ' www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
