a23cfda3480591532a55a43f87361ce08677292b
Iyiola-coding <iyiolache@gmail.com>
2025-01-17 09:17:09 -0800

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# automating configuration with puppet
+# Install Nginx with puppet
+
+package  { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@iyitech permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => runinig,
+  require => package['nginx'],
+}
